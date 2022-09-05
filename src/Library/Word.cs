026b5e3afb1e56a64f0af3f5e007985b1a12458f
IgnacioVillarreal31 <IGNACIO.VILLARREAL@correo.ucu.edu.uy>
2022-09-05 10:13:12 -0300

    Actualizacion

--- src/Library/Word.cs
+++ src/Library/Word.cs
@@ -9,23 +9,13 @@ namespace WordsPhrases
 {
     public class Word
     {
-        // Para uso interno de la propiedad Text.
         private string text;
 
-        /// <summary>
-        /// Crea una nueva palabra con el texto recibido como argumento.
-        /// </summary>
-        /// <param name="text">El texto de la palabra.</param>
         public Word(string text)
         {
-            this.Text = text;
+            this.Text = text.Trim();
         }
 
-        /// <summary>
-        /// Obtiene o establece el texto de la palabra. El texto no tiene espacios al comienzo ni al final -los espacios
-        /// son removidos al asignar el texto-.
-        /// </summary>
-        /// <value>El texto de la palabra</value>
         public string Text
         {
             get
